2380d6701f54e128c86c3a4d21363d083a066c35
DarkNight <truonguit2010@gmail.com>
2021-06-14 21:11:57 +0700

    - Init common Rule pattern

---
+++ Runtime/RulePattern/GenericRuleEngine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace RealbizGames.RulePattern
+{
+    public class GenericRuleEngine<T, E> : IRuleEngine<T, E>
+    {
+        private List<IRule<T, E>> rules = new List<IRule<T, E>>();
+
+        public void AddRule(IRule<T, E> rule)
+        {
+            rules.Add(rule);
+        }
+
+        public T Execute(E expression)
+        {
+            foreach (IRule<T, E> rule in rules)
+            {
+                if (rule.Valuate(expression))
+                {
+                    return rule.Execute(expression);
+                }
+            }
+            return default;
+        }
+    }
+}
---
+++ Runtime/RulePattern/IRuleEngine.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RealbizGames.RulePattern
+{
+    public interface IRuleEngine<T, E>
+    {
+        void AddRule(IRule<T, E> rule);
+
+        T Execute(E expression);
+    }
+}
